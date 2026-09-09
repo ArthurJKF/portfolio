4648338d26613ac4117fb1f970c8a3223d26cacd
ArthurJKF <arthurjac.kf@gmail.com>
2026-09-09 03:18:41 -0300

    rota à página Colecao criada

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/colecao" element={<Colecao />} />
       </Routes>
 
       <Footer />
